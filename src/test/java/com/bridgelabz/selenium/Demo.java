e034703db707568336f309050b174936b14398e1
TuChawat <tushar1chawat@gmail.com>
2021-07-17 12:27:59 +0530

    first commit

---
+++ src/test/java/com/bridgelabz/selenium/Demo.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.selenium;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+    @Test
+    public void printHelloWorld(){
+        int i;
+        for (i = 0; i <= 10; i++){
+            System.out.println("Hello World");
+        }
+    }
+}
